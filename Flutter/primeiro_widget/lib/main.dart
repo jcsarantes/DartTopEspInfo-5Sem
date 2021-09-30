dc36b35f1ad5d4e11a84b95ea95fd33288ed8c64
janai <prof.janaine@gmail.com>
2021-09-30 08:58:38 -0300

    Exemplos da Aula 07

---
+++ Flutter/primeiro_widget/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome to Flutter'),
+        ),
+        body: const Center(
+          child: Text('Hello, World!,'),
+        ),
+      ),
+    );
+  }
+}
